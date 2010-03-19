b2f1b96dcfb18c299d0f60cad7ac98a427e96aba
leszgod081 <leszgod081@ba71ee55-0f36-41dd-98d7-ab351bc57a59>
2010-03-19 10:49:24 +0000

    * fixed the spiral matrix program to conform with the teacher's requirements
    git-svn-id: https://subversion.assembla.com/svn/iqpk@29 ba71ee55-0f36-41dd-98d7-ab351bc57a59

--- pk2-lab/matrix.c
+++ pk2-lab/matrix.c
@@ -3,27 +3,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define R	4
+#define R	7
 
 int tab[R * R];
 
 int main(int argc, char *argv[]) {
-	int i, j, k, l;
+	int i, j = 0, k = 0;
 	int xlen = R, ylen = R - 1;
 	int xdir = 1, ydir = 1;
 	
 	memset(tab, -1, sizeof(tab));
-		
-	for (i = 0, k = 0, l = 0; i < R; i++) {
-		for (j = 0; j < xlen; j++, k += xdir, l++)
-			tab[k] = l;
-		k += (R - 1) * xdir;
+
+	while (ylen > 0 || xlen > 0) {
+		for (i = 0; i < xlen; i++, j += xdir, k++)
+			*(tab + j) = k;
+		j += (R - 1) * xdir;
 		xlen--;
 		xdir = -xdir;
 		
-		for (j = 0; j < ylen; j++, k += R * ydir, l++)
-			tab[k] = l;
-		k += (R + 1) * -ydir;
+		for (i = 0; i < ylen; i++, j += R * ydir, k++)
+			*(tab + j) = k;
+		j += (R + 1) * -ydir;
 		ylen--;
 		ydir = -ydir;
 	}
