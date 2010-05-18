1a8973dc4828055baba78d38e404411424077833
leszgod081 <leszgod081@ba71ee55-0f36-41dd-98d7-ab351bc57a59>
2010-05-18 23:27:28 +0000

    * ac130: added missing project file changes * ac130: preliminary overlay blur effect
    git-svn-id: https://subversion.assembla.com/svn/iqpk@59 ba71ee55-0f36-41dd-98d7-ab351bc57a59

--- ac130/compositor_fs.glsl
+++ ac130/compositor_fs.glsl
@@ -4,16 +4,43 @@ uniform sampler2D frames[6];	// MUST be kept in sync with 1 + FRAME_TRACE!!!
 
 vec4 get_view(vec2 st) {
 	//return texture2D(frames[6], gl_TexCoord[0].st);
-	return 0.705 * texture2D(frames[5], gl_TexCoord[0].st)
-		+ 0.105 * texture2D(frames[4], gl_TexCoord[0].st)
-		+ 0.085 * texture2D(frames[3], gl_TexCoord[0].st)
-		+ 0.065 * texture2D(frames[2], gl_TexCoord[0].st)
-		+ 0.045 * texture2D(frames[1], gl_TexCoord[0].st)
-		+ 0.025 * texture2D(frames[0], gl_TexCoord[0].st);
+	return 0.705 * texture2D(frames[5], st)
+		+ 0.105 * texture2D(frames[4], st)
+		+ 0.085 * texture2D(frames[3], st)
+		+ 0.065 * texture2D(frames[2], st)
+		+ 0.045 * texture2D(frames[1], st)
+		+ 0.025 * texture2D(frames[0], st);
+}
+
+const float pixOfs = 0.001;
+vec4 get_overlay(vec2 st) {
+	// left part of an order 4 Gaussian blur kernel
+	return 0.0052 * (
+		// row 1
+		texture2D(overlay, st + vec2(-2.0 * pixOfs, -2.0 * pixOfs))
+		+ 4.0 * texture2D(overlay, st + vec2(-pixOfs, -2.0 * pixOfs))
+		+ 4.0 * texture2D(overlay, st + vec2(0.0, -2.0 * pixOfs))
+		// row 2
+		+ 4.0 * texture2D(overlay, st + vec2(-2.0 * pixOfs, -pixOfs))
+		+ 16.0 * texture2D(overlay, st + vec2(-pixOfs, -pixOfs))
+		+ 24.0 * texture2D(overlay, st + vec2(0.0, -pixOfs))
+		// row 3
+		+ 6.0 * texture2D(overlay, st + vec2(-2.0 * pixOfs, 0.0))
+		+ 24.0 * texture2D(overlay, st + vec2(-pixOfs, 0.0))
+		+ 36.0 * texture2D(overlay, st + vec2(0.0, 0.0))
+		// row 4
+		+ 4.0 * texture2D(overlay, st + vec2(-2.0 * pixOfs, pixOfs))
+		+ 16.0 * texture2D(overlay, st + vec2(-pixOfs, pixOfs))
+		+ 24.0 * texture2D(overlay, st + vec2(0.0, pixOfs))
+		// row 5
+		+ texture2D(overlay, st + vec2(-2.0 * pixOfs, 2.0 * pixOfs))
+		+ 4.0 * texture2D(overlay, st + vec2(-pixOfs, 2.0 * pixOfs))
+		+ 4.0 * texture2D(overlay, st + vec2(0.0, 2.0 * pixOfs))
+		);
 }
 
 void main() {
-	vec4 ov = texture2D(overlay, gl_TexCoord[0].st);
+	vec4 ov = get_overlay(gl_TexCoord[0].st);
 	vec4 view = get_view(gl_TexCoord[0].st);
 	gl_FragColor = vec4(ov.rgb * ov.a + view.rgb * (1.0 - ov.a), 1.0);
 }
